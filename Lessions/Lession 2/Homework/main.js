578e1f7afa5c447e44837f48e9055e3998f537f7
GiorgadzeG'rungit pullexitclean <giorgadze48@gmail.com>
2023-10-09 18:48:51 +0400

    update

--- Lessions/Lession 2/Homework/main.js
+++ Lessions/Lession 2/Homework/main.js
@@ -24,5 +24,58 @@ for(let i = 0; i < users.length; i++){
     }
 }
 console.log("Youngest user is", youngestName,'Age:', youngestAge) // Yangest user is Lasha Age: 21
+console.log(users)
 
 
+// დაწერე ფუნქცია, რომელიც პარამეტრად
+// მიიღებს user ობიექტს და დააბრუნებს იგივე
+// მნიშვნელობების მქონე ახალ
+// (განსხვავებულ) ობიექტს
+
+let user = {
+    name: "Giorgi",
+    lastname: "Giorgadze",
+    age: 23,
+}
+
+
+
+
+
+
+// დაწერე პროგრამა, სადაც ორი a და b
+// მომხმარებლები აგორებენ კამათელს მანამ,
+// სანამ არ გაგორდება, რომელიც უფრო
+// ნაკლებ ცდაში გააგორებს სამიანს ის არის
+// გამარჯვებული
+
+
+
+function kamateli(){
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+player1win = false
+player2win = false
+
+function game(){
+    let player1win = kamateli()
+    console.log("player 1:", player1win)
+    let player2win = kamateli()
+    console.log("player 2:", player2win)
+    if(player1win == 3 && player2win == 3) {
+        return "tie"} else
+    if(player1win == 3){
+        player1win = true
+        return ("Player 1 win")
+    }else if(player2win == 3){
+        player1win = true
+        return ("Player 2 win")
+    } else if(player1win == 3 || player2win == 3){
+        return "tie"
+    } else {
+        return "tie"
+    }
+    }
+
+console.log(game())  // sxva ragaca gavakete...
