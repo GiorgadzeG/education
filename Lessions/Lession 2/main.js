ec0801026a23c3f12bca287651a525813265eef0
GiorgadzeG'rungit pullexitclean <giorgadze48@gmail.com>
2023-10-09 14:28:11 +0400

    update

--- Lessions/Lession 2/main.js
+++ Lessions/Lession 2/main.js
@@ -1,4 +1,9 @@
 console.log(typeof null) // object
+// for in {} object
+// for of [] array
+// for(const ABC in/of masivis saxeli){
+// console.log (ABC)
+//}
 
 const user = {
     name: 'James',
@@ -82,3 +87,86 @@ for(let i = 0; i < arr1.length; i++) {
                     // 30
                     // null
 
+for( let i = 0; i < arr1.length; i++) {
+    console.log(arr[i] + ' ' + 'is moving')
+}                   // James is moving
+                    // Giorgi is moving
+                    // Giga is moving
+                    // undefined is moving
+//
+//
+const arr2 = ['James', 'Giorgi', 'Giga', 'Temo']
+for(const name of arr2) {
+    console.log(name)
+}                   // James
+                    // Giorgi
+                    // Giga
+                    // Temo
+//
+//
+const C = {
+name: 'James',
+lastName: 'Bond',
+userName: '007'
+}
+
+for (const f in C){
+    console.log(C[f])
+}                   // James
+                    // Bond 
+                    // 007
+//
+//
+function randomNumber(){
+    return parseInt(Math.random() * 10) + 1
+}
+
+let result = randomNumber()
+console.log(result) // random 1 dan 10 mde
+
+while(result !==9){
+    result = randomNumber()
+    console.log(result)
+}                   // random sanam 9 ar iqneba
+//
+//
+do{
+    result = randomNumber()
+        console.log(result)
+} while(result!==9) // igive
+//
+//
+const arr3 = ['James', 'Giorgi', 'Giga', 'Temo']
+
+arr3.forEach(function(element, index){
+    console.log(index + ' is equal to ' + element)
+})                  // 0 is equal to James
+                    // 1 is equal to Giorgi
+                    // 2 is equal to Giga
+                    // 3 is equal to Temo
+                    // *results ar abrunebs
+
+
+const resMap = arr3.map(function(racginda, index){
+    return (index + ' is equal to ' + racginda)
+})
+console.log(resMap) // 0 is equal to James
+                    // 1 is equal to Giorgi
+                    // 2 is equal to Giga
+                    // 3 is equal to Temo
+
+
+const array = [1, 5, 3, 7, 4, 3, 2, 9]
+const resFilter = array.filter(function(whatuwant){
+    return whatuwant > 5
+})
+console.log(array)     // [1, 5, 3, 7, 4, 3, 2, 9]
+console.log(resFilter) // [7, 9]
+
+const resFind = array.find(function(el){
+    return el > 5
+})
+
+console.log(resFind) // 7
+
+// map filteri da find abruneben axal mnishvnelobs ise ro ar cvlian dziritad masivs
